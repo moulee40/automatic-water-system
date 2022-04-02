f57bf213a096ce99004b0558550cc9b4ec28183d
Moulee <moulee40@gmail.com>
2022-04-02 15:21:45 +0530

    circuit diagram commit

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -12,6 +12,8 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
 import Slide from '@material-ui/core/Slide';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -47,8 +49,19 @@ export default function FullScreenDialog({open,handleClose}) {
           </Toolbar>
         </AppBar>
         <List className='flex flex-1'>
-            <div className="flex m-auto w-96 h-80 border-2 border-solid border-black"/>
-
+            <div className="flex m-auto w-1/2 h-80 border-2 border-solid border-black rounded relative">
+              <div className='h-80 border-l-2 border-black border-solid absolute left-1/4'></div>
+              <div className='h-80 border-l-2 border-black border-solid absolute left-1/2'></div>
+              <div className='h-80 border-l-2 border-black border-solid absolute left-3/4'></div>
+              <div className="border-black rounded-full border-2 border-solid w-14 h-14 absolute left-[21.5%] top-1/3 bg-blue-100 pl-2 pt-3">1018</div>
+              <div className="border-black rounded-full border-2 border-solid w-14 h-14 absolute left-[46.5%] top-1/3 bg-blue-100 pl-2 pt-3">1019</div>
+              <div className="border-black rounded-full border-2 border-solid w-14 h-14 absolute left-[71.5%] top-1/3 bg-blue-100 pl-2 pt-3">1020</div>
+              <div className="border-black rounded-full border-2 border-solid w-14 h-14 absolute left-[96.5%] top-1/3 bg-blue-100 pl-2 pt-3">1021</div>
+              <ArrowBackIosIcon className='text-blue-600 absolute top-[-4%] left-[10%]'/>
+              <ArrowForwardIosIcon className='text-blue-600 absolute top-[96.5%] left-[35%]'/>
+              <ArrowBackIosIcon className='text-blue-600 absolute top-[-4%] left-[60%]'/>
+              <ArrowForwardIosIcon className='text-blue-600 absolute top-[96.5%] left-[86%]'/>
+            </div>
         </List>
       </Dialog>
     </div>
