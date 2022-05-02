c179f4ddf5aaccf50132fd83f4a592ec20e9a9ed
Moulee <mouleeswaran@agentz.ai>
2022-05-02 18:27:43 +0530

    5 digit trim commit

--- src/components/DetailBox.js
+++ src/components/DetailBox.js
@@ -6,12 +6,16 @@ class DetailBox extends React.Component {
 s
   render() {
       const {marginTopLg,rightArrow,pvValue,ovValue,title,marginRightLg} = this.props;
+      const pvStringValue = pvValue.toString();
+      const ovStringValue = ovValue.toString();
+      const finalPvValue = Number(pvStringValue.slice(0, 6));
+      const finalOvValue = Number(ovStringValue.slice(0, 6));
     return (
         <div className={`flex flex-col w-64 h-28 border-2 border-solid border-gray-200 rounded relative m-4 shadow-lg ${marginTopLg ? 'mt-28' :''} ${marginRightLg ? 'mr-14' :''}`}>
           <div className='flex justify-center items-center flex-1 border-b-2 border-solid border-gray-400'>{title}(m3/h)</div>
           <div className='flex flex-1'>
-            <div className='flex justify-center items-center flex-1 text-center border-r-2 border-solid border-gray-400'><span className='text-sm'>PV Value : </span>{pvValue}</div>
-            <div className='flex justify-center items-center flex-1 text-center'><span className='text-sm'>OV Value : </span>{ovValue}</div>
+            <div className='flex justify-center items-center flex-1 text-center border-r-2 border-solid border-gray-400'><span className='text-sm'>PV Value : </span>{finalPvValue}</div>
+            <div className='flex justify-center items-center flex-1 text-center'><span className='text-sm'>OV Value : </span>{finalOvValue}</div>
           </div>
         {rightArrow && <>
           <div className={`w-[1rem] border-t-2 border-gray-500 border-solid absolute top-[75%] left-full ${marginRightLg ? 'w-[58px]' :'w-[1rem]'}`}>
